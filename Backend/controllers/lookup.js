a14e024eb2a8a5d7e90eb0ac00545180d7491cfe
VarunS121 <f20211054@pilani.bits-pilani.ac.in>
2023-06-19 13:09:42 +0530

    Trired runncing CLI tools

--- Backend/controllers/lookup.js
+++ Backend/controllers/lookup.js
@@ -10,21 +10,17 @@ const phoneNoLookup = async (req, res) => {
 
 const usernameLookup = async (req, res) => {
     const target = req.query.target
+    const result = {}
     if (target) {
         console.log(`Searching for ${target}`);
-        await exec(`python3 ./sherlock/sherlock ${target}`, (err, output) => {
+        await exec(`echo ${target}`, (err, output) => {
             if (err) {
                 console.error(err)
                 res.render('username', { result: err.stdout })
             }
             console.log(output)
-            res.render('username', { result: output })
+            res.render('username', { output })
         })
-        // const command = exec (`timeout 30s python3 ./sherlock/sherlock ${target}`);
-        // command.on('close', (code) => {
-        //     console.log(code);
-        // })
-        // setTimeout(() => {command.kill()}, 10000)
     }
     res.render('username')
 }
