fe4cfec4674280c2ffbf0d03e9fd419620443334
Rohan <prohan1408@gmail.com>
2023-06-19 19:16:27 +0530

    Added loading gif and implemented loading js

---
+++ Frontend/js/script.js
@@ -0,0 +1,22 @@
+function search() {
+    var target = document.getElementById('target').value;
+    var output = document.getElementById('output');
+    var loadingImage = document.getElementById('loading-image');
+  
+    // Hide the output and show the loading image
+    output.style.display = 'none';
+    loadingImage.style.display = 'block';
+  
+    // Simulate a delay to represent the actual search process
+    setTimeout(function () {
+      // Perform OSINT search using target information
+  
+      // Display the results
+      output.textContent = "Results for: " + target;
+  
+      // Hide the loading image and show the results
+      loadingImage.style.display = 'none';
+      output.style.display = 'block';
+    }, 2000); // Simulated 2-second delay for demonstration purposes
+  }
+  
