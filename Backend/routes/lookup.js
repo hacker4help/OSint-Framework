8915ed963bc5bd0462558b88675be36fb15c5b72
VarunS121 <f20211054@pilani.bits-pilani.ac.in>
2023-06-29 12:31:21 +0530

    Added tools for all lookups

--- Backend/routes/lookup.js
+++ Backend/routes/lookup.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const { websiteLookup, phoneNoLookup, usernameLookup, ipLookup, emailLookup } = require('../controllers/lookup')
+const { websiteLookup, phoneNoLookup, usernameLookup, ipLookup, emailLookup, aboutUs, info } = require('../controllers/lookup')
 
 
 router.route('/website').get(websiteLookup);
@@ -9,5 +9,7 @@ router.route('/phone').get(phoneNoLookup);
 router.route('/username').get(usernameLookup);
 router.route('/ip').get(ipLookup);
 router.route('/email').get(emailLookup);
+router.route('/about-us').get(aboutUs)
+router.route('/info').get(info)
 
 module.exports = router
